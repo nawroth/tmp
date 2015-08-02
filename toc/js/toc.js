ba51392cc362da598011ddfe07b645f3ceb2efe8
Anders Nawroth <anders@neotechnology.com>
2015-08-02 14:12:55 +0200

    Add toc experiment.

---
+++ toc/js/toc.js
@@ -0,0 +1,70 @@
+/**
+ * Licensed to Neo Technology under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Neo Technology licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+function initToc()
+{
+  var HIDDEN_CLASS = 'hidden-section-toc';
+  var VISIBLE_CLASS = 'visible-section-toc';
+  var $pageContent = $( '#content' );
+  var $wrapper = $( 'div.toc', $pageContent );
+
+  if ( $wrapper.length === 0 )
+  {
+    return;
+  }
+  var size = $( 'li', $wrapper ).length;
+  if ( size < 7 )
+  {
+    return;
+  }
+
+  $wrapper.addClass( VISIBLE_CLASS );
+
+  var $outerList = $( 'ul.toc', $wrapper );
+  var hasInnerList = $( 'ul', $outerList ).length > 0;
+
+  var $heading = $( 'p', $wrapper );
+  if ( $heading.length === 0 )
+  {
+    $heading = $( '<p/>' ).prependTo( $wrapper );
+  }
+
+  var $hideButton = $( '<label class="btn btn-default btn-xs" title="Hide table of contents"><input type="radio" name="tocState" autocomplete="off">Hide</label>' ).click(function(){
+    $wrapper.addClass( HIDDEN_CLASS ).removeClass( VISIBLE_CLASS );
+  });
+  var $showButton = $( '<label class="btn btn-default btn-xs active" title="Show table of contents"><input type="radio" name="tocState" autocomplete="off" checked>Show</label>' ).click(function(){
+    $wrapper.addClass( VISIBLE_CLASS ).removeClass( HIDDEN_CLASS );
+  });
+  var $detailButton = $( '<label class="btn btn-default btn-xs" title="Show detailed table of contents"><input type="radio" name="tocState" autocomplete="off">Details</label>' ).click(function(){
+    $wrapper.removeClass( VISIBLE_CLASS ).removeClass( HIDDEN_CLASS );
+  });
+
+  var $buttons = $( '<div class="btn-group" data-toggle="buttons"></div>' )
+    .append( $hideButton )
+    .append( $showButton );
+  if ( hasInnerList )
+  {
+    $buttons.append( $detailButton );
+  }
+  $heading.append( $buttons );
+}
+
+$( document ).ready( initToc );
+
+
